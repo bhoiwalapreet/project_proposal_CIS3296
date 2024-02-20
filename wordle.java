550862ae40a5e4df598c9daecda3f6b90bae0873
bhoiwalapreet <tun38627@temple.edu>
2024-02-20 13:20:53 -0500

    wrote code to demo simple guess and check

--- wordle.java
+++ wordle.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class wordle {
-}
+public class wordle {
+        public static void main(String[] args) {
+            String secretWord = "coder";
+            String userGuess = "coder"; // In a real application, you'd get this from user input
+
+            checkGuess(secretWord, userGuess);
+        }
+
+        public static void checkGuess(String secretWord, String guess) {
+            if (guess.equals(secretWord)) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Incorrect");
+            }
+        }
+    }
